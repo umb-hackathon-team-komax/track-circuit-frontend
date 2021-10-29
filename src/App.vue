fa459b33f293909c49481e1abd775c91c969508d
noahziltener <noahziltener@pop-os.localdomain>
2021-10-29 15:29:01 +0200

    added vue code

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from './components/Header.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Header
+  }
+});
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="px-4 container mx-auto m-4">
+  <div class="text-left mb-8">
+    <h1 class="text-gray-800 md:text-5xl font-bold mb-2">
+      Track Circuit
+    </h1>
+
+    <!-- change "border-indigo-500" to change the color -->
+    <div class="border-indigo-500 border-b-2 border-t-2 w-32 md:w-40 rounded"></div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
